f011b8dbac50c530cefc55d46fd831bb444de749
arukreid0609 <y.sasaki.roppongi@gmail.com>
2023-11-02 16:30:36 +0900

    First Commit

---
+++ Assets/Scripts/SimpleFollow.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFollow : MonoBehaviour
+{
+    Vector3 diff;
+
+    public GameObject target;
+    public float followSpeed;
+
+    void Start()
+    {
+        diff = target.transform.position - transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(
+            transform.position,
+            target.transform.position - diff,
+            Time.deltaTime * followSpeed
+        );
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
