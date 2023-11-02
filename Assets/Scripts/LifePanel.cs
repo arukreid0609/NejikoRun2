1e62586764209b400d23ec3e305bcc28b04ad9bf
arukreid0609 <y.sasaki.roppongi@gmail.com>
2023-11-02 20:41:15 +0900

    304pまでの内容

---
+++ Assets/Scripts/LifePanel.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifePanel : MonoBehaviour
+{
+    public GameObject[] icons;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
