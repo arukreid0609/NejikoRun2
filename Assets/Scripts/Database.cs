0c503f3f93cfd5b0b0ef9ef7746dcf104255e5ec
sasakiy916 <sasaki3y98521j@gmail.com>
2023-11-05 17:51:40 +0900

    データベース連携 追加

---
+++ Assets/Scripts/Database.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Database : MonoBehaviour
+{
+    [Serializable]
+    public class Result
+    {
+        public User[] result;
+        [Serializable]
+        public class User
+        {
+            public int id;
+            public string name;
+            public int record;
+
+        }
+    }
+    private void Start()
+    {
+        StartCoroutine(GetRanking());
+    }
+
+    IEnumerator GetRanking()
+    {
+        string url = "http://localhost/webapi/webapi.py";
+        WWWForm form = new WWWForm();
+
+        form.AddField("name", "nejiko");
+        form.AddField("record", 9999);
+
+        using (UnityWebRequest uwr = UnityWebRequest.Post(url, form))
+        {
+            yield return uwr.SendWebRequest();
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                Debug.LogError("Error: " + uwr.error);
+            }
+            else
+            {
+                string responseText = uwr.downloadHandler.text;
+                Result result = JsonUtility.FromJson<Result>(responseText);
+                Debug.Log(result.result[0].name);
+                Debug.Log(result.result[0].id);
+                Debug.Log(result.result[0].record);
+                // Debug.Log("Response: " + responseText);
+            }
+        }
+    }
+}
